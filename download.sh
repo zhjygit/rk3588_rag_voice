8498a8e4ede00bd57695e293f814dbe4ddb38673
zhjygit <44870751+zhjygit@users.noreply.github.com>
2025-06-05 08:49:23 +0800

    Update download-paraphrase-multilingual-MiniLM-L12-v2.sh

--- download.sh
+++ download.sh
@@ -3,3 +3,6 @@
 git lfs install
 
 git clone https://huggingface.co/sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2
+
+#国内用户
+#可以从https://hf-mirror.com/sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2下载，即hf-mirror，下载方法自行搜索hf-mirror下载即可。
